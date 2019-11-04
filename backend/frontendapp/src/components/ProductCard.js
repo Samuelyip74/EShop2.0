8e9d83f32d12af7822fc068e0456eff04991ec8b
Samuel <samuel.yip@live.com>
2019-11-04 23:04:42 +0800

    commit

--- backend/frontendapp/src/components/ProductCard.js
+++ backend/frontendapp/src/components/ProductCard.js
@@ -3,24 +3,42 @@ import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { Link, Route,useParams } from 'react-router-dom'
-import Grid from '@material-ui/core/Grid';
+import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
   card: {
-      // TODO: Calc width divide by 2
-      minWidth: 180,
-      maxWidth: 345,
+    minWidth: 375,
   },
   media: {
-      height: 140,
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
   },
 }));
 
@@ -30,10 +48,13 @@ export default function ProductCard(props) {
   const [data, setdata] = useState( [] );
   const [loading, setloading] = useState(true);
   const [expanded, setExpanded] = useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
-  useEffect(() => { 
+  useEffect(() => {
     axios.get(`http://127.0.0.1:8000/api/product/variant/` + id)
-        .then(res => {           
+        .then(res => {
         setdata(res.data);
         setloading(false);
     })
@@ -45,35 +66,50 @@ export default function ProductCard(props) {
     )
   } else {
     return (
-        <div className={classes.root} style={{marginLeft:5,marginRight:5}}>
-        <Grid 
-            container
-            direction="row"
-            justify="space-evenly"
-            alignItems="center"
-            spacing={1}>
-            {data.map(item => (
-            <Grid item xs={12} sm={12} key={item.id}>
-                <Link to={`/app/product/${item.id}`} style={{textDecoration:'none'}}>
-                <Card className={classes.card}>
-                    <CardActionArea>
-                        <CardMedia
-                        className={classes.media}
-                        image={"http://127.0.0.1:8000" + item.images[0].image}
-                        title={item.sku}
-                        />
-                        <CardContent style={{display:'flex',}}>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            {item.sku}
-                        </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-                </Link>
-            </Grid>
-            ))}
-        </Grid>
-        </div>
-    );
+      <Card className={classes.card} style={{marginBottom:50}}>
+        <CardMedia
+          className={classes.media}
+          image={"http://127.0.0.1:8000" + data[0].images[0].image}
+          title={data[0].sku}
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p" style={{textAlign:"left"}}>
+            {data[0].sku}
+          </Typography>         
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph style={{textAlign:"left"}}>Method:</Typography>
+            <Typography paragraph style={{textAlign:"left"}}>
+              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+              minutes.
+            </Typography>
+            <Typography paragraph style={{textAlign:"left"}}>
+              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+              heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+              browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+              and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+              pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+              saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    )
   }
 }
